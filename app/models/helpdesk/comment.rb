674cda461b41fb1369f9d638a79e09134a76840f
John Beynon <john@beynon.org.uk>
2012-04-24 13:59:41 +0100

    Comment tests

--- app/models/helpdesk/comment.rb
+++ app/models/helpdesk/comment.rb
@@ -2,5 +2,6 @@ module Helpdesk
   class Comment < ActiveRecord::Base
     attr_accessible :author_id, :comment, :public, :ticket_id
     belongs_to :author, :class_name => Helpdesk.user_class.to_s
+    belongs_to :ticket
   end
 end
