e3bda69249399630c7943d71c969d10a5f81389a
John Beynon <john@beynon.org.uk>
2012-04-26 10:21:07 +0100

    Removing files

--- lib/generators/helpdesk/install/templates/helpdesk_admin_migration.rb
+++
@@ -1,7 +0,0 @@
-class AddHelpdeskAdmin < ActiveRecord::Migration
-
-  def change
-    add_column <%= user_class.constantize.table_name.to_sym.inspect %>, :helpdesk_admin, :boolean, :default => false
-  end
-
-end
