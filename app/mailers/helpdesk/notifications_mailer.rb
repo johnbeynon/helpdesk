75d2406a589b305c5920da286c4f1e6e4ed33cde
Wacław Łuczak <waclaw@luczak.it>
2013-06-20 12:35:13 +0200

    fixed email address (after create comment by helpdesk)

--- app/mailers/helpdesk/notifications_mailer.rb
+++ app/mailers/helpdesk/notifications_mailer.rb
@@ -29,7 +29,7 @@ class Helpdesk::NotificationsMailer < ActionMailer::Base
   def comment_by_helpdesk_notification(comment)
     @comment = comment
     mail(:subject=>"#{Helpdesk.helpdesk_name} | #{comment.ticket.subject}",
-         :to => comment.author.email)
+         :to => comment.ticket.requester.email)
   end
 
   def comment_by_helpdesk_confirmation(comment)
